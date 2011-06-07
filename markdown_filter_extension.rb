e62e60179660e808d058b028466a7cce49ec6230
johnmuhl <git@johnmuhl.com>
2011-06-06 20:33:09 -0500

    3 digit version

--- markdown_filter_extension.rb
+++ markdown_filter_extension.rb
@@ -1,7 +1,7 @@
 require "bluecloth"
 
 class MarkdownFilterExtension < Radiant::Extension
-  version "1.0"
+  version "1.0.0"
   description "Allows you to compose page parts or snippets using the Markdown text filter."
   url "http://daringfireball.net/projects/markdown/"
 
