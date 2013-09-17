e6ab1de7ef60385ad61db79a959a483ac151044c
Jim Gay <jim@saturnflyer.com>
2013-09-17 10:20:10 -0400

    update version and remove 1.9.2 testing

--- lib/radiant-markdown_filter-extension/version.rb
+++ lib/radiant-markdown_filter-extension/version.rb
@@ -1,3 +1,3 @@
 module RadiantMarkdownFilterExtension
-  VERSION = "1.0.2"
+  VERSION = "1.0.3"
 end
--- markdown_filter_extension.rb
+++ markdown_filter_extension.rb
@@ -1,9 +1,9 @@
 require "bluecloth"
 
 class MarkdownFilterExtension < Radiant::Extension
-  version "1.0.0"
+  version RadiantMarkdownFilterExtension::VERSION
   description "Allows you to compose page parts or snippets using the Markdown text filter."
-  url "http://daringfireball.net/projects/markdown/"
+  url "http://github.com/radiant/radiant-markdown_filter-extension/"
 
   def activate
     MarkdownFilter
